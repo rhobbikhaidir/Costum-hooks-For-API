a98de81806ad89f805cf2596f6e3820ef647882f
rhobbikhaidir <rhobbie09@gmail.com>
2022-05-19 08:53:51 +0700

    Finish Course Costum hooks for api(Reuseable)

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import NewTask from "./components/NewTask/NewTask";
+import Tasks from "./components/Tasks/Tasks";
+import useHttp from "./hooks/use-http";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    const transfromTasks = (taskObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+      setTasks(loadedTasks);
+    };
+    fetchTasks(
+      { url: "https://tesapp-d0cbe-default-rtdb.firebaseio.com/tasks.json" },
+      transfromTasks
+    );
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
